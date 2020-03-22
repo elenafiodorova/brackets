ba5ba95a3f525a10f1551a23140f98a9574db38d
elenafiodorova <fiodorova.elena.25.08@gmail.com>
2020-03-22 23:44:49 +0300

    feat: add solution task brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    for (let i = 0; i <= bracketsConfig.length - 1;) {
+        let col = bracketsConfig[i].join('');
+        if (str.includes(col)) {
+            str = str.replace(col, '');
+            i = 0;
+        } else {
+            i = i + 1;
+        }
+    }
+    if (str.length == 0) {
+        return true;
+    } else {
+        return false;
+    }
+};
